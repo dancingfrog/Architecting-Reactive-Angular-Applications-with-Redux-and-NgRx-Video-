aad4e5a85bf4fad64c3284079dbfa7372e6fb721
John <john@real-currents.com>
2020-03-27 14:29:16 -0400

    improve state tracking in reducer example

--- section1/part4/concepts/action.js
+++ section1/part4/concepts/action.js
@@ -1,12 +1,13 @@
-// action 
+// action
 
 const action = { type: 'CREATE_ITEM', payload: 'new item' };
+console.log(action);
 
 // action creator
 
-const create = (item) => ({ type: 'CREATE_ITEM', payload: item });
+const actionFactory = (action_type, action_item) => ({ type: action_type, payload: action_item });
 
-create('item1');
-create('item2');
-create('item3');
-create('item4');
+console.log(actionFactory('CREATE_ITEM',  'item1'));
+console.log(actionFactory('CREATE_ITEM', 'item2'));
+console.log(actionFactory('CREATE_ITEM', 'item3'));
+console.log(actionFactory('REMOVE_ITEM', 'item1'));
--- section1/part4/concepts/reducer.js
+++ section1/part4/concepts/reducer.js
@@ -3,14 +3,26 @@
 function reducer(prevState = [], action) {
   switch(action.type) {
     case 'CREATE_ITEM':
-      return [ ...prevState, {...action.payload}];
+      return [ ...prevState, [].concat(...prevState, action.payload) ];
+
+    case 'REMOVE_ITEM':
+      const mostRecentState = prevState[prevState.length - 1];
+      const itemIndex = mostRecentState.findIndex((v, i, a) => action.payload.title === v.title);
+      return [ ...prevState, [].concat(...(mostRecentState.slice(0, itemIndex)), ...(mostRecentState.slice(itemIndex + 1))) ];
+
     default:
       return prevState;
   }
 }
 
-let state = reducer([], { type: 'CREATE_ITEM', payload: { title: 'new item' } });
-console.log('state', state);
+let state = [];
+console.log('init state', state);
 
-state = reducer([], { type: 'CREATE_ITEM', payload: { title: 'new item2' } });
-console.log('state', state);
+state = reducer([], { type: 'CREATE_ITEM', payload: { title: 'new item' } });
+console.log('update state', state);
+
+state = reducer(state, { type: 'CREATE_ITEM', payload: { title: 'new item2' } });
+console.log('update state', state);
+
+state = reducer(state, { type: 'REMOVE_ITEM', payload: { title: 'new item' } });
+console.log('update state', state);
